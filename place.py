69e78ede320284130943a2a577574f4c63b58789
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-05-25 10:32:33 +0900

    * place.py: python version of old script.

---
+++ place.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+'''
+This script places every dot file to their destination.
+'''
+
+DATE = "Saturday, May 25 2013"
+AUTHOR = "Yagnesh Raghava Yakkala"
+WEBSITE = "http://yagnesh.org"
+LICENSE = "GPL v3 or later"
+
+import os
+import inspect
+import re
+import shutil
+
+this_file_path = os.path.abspath(os.path.split(inspect.getfile(
+    inspect.currentframe()))[0])
+ignores = [".git", "README.org", ".gitignore"]
+
+
+def find_attri(f):
+    n = d = None
+    with open(f) as fh:
+        for line in fh:
+            if re.match(r'.\+DEST=(.*)', line):
+                d = re.sub(r'.\+DEST=(.*)', r'\1', line)
+            if re.match(r'.\+FNAME=(.*)', line):
+                n = re.sub(r'.\+FNAME=(.*)', r'\1', line)
+            if d and n:
+                break
+    return [n, d]
+
+
+def place_file(f):
+    [fname, d] = find_attri(f)
+    if fname and d:
+        dest = os.path.expandvars(d.rstrip()+fname.rstrip())
+        print('   %s => %s' % (f, dest))
+        try:
+            shutil.copy(f,dest)
+        except IOError:
+            print('*FAILED*: %s => %s' % (f, dest))
+    else:
+        print('*FAILED*: %s => ???' % f)
+
+def main():
+    os.chdir(this_file_path)
+    reg = re.compile("|".join(map(re.escape, ignores)))
+
+    for f in os.listdir(this_file_path):
+        if f == os.path.split(__file__)[1] or reg.search(f):
+            continue
+        place_file(f)
+
+
+if __name__ == '__main__':
+    main()
